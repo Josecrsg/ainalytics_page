025606ca2655dbd16e22ab9b0857bd450630f050
Jose <josecrsg@gmail.com>
2023-10-24 17:32:28 +0200

    sigin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js"
 import bcryptjs from "bcryptjs"
+import jwt from 'jsonwebtoken'
+
 
 export const signup = async (req, res, next) =>{
     const { username, email, password} = req.body;
@@ -13,3 +15,19 @@ export const signup = async (req, res, next) =>{
         next(error)
     }
 };
+
+export const signin = async (req, res, next) =>{
+    const {email, password} = req.body;
+    try {
+        const validUser = await User.findOne({email});
+        if (!validUser) return next(errorHandler(401, 'User not four'))
+        const validPassword = bcryptjs.compareSync(password, validUser.password)
+        if(!validPassword) return next(errorHandler(401, 'Wrong credentials!'))
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+        /**CON LA LINEA INFERIOR CONSEGUIMOS DESESTRUCTURAR LA INFORMACION Y ELIMINAMOS LA INFORMACION DE PASSWORD PARA QUE SE NO PUEDA ACCEDER */
+        const {password: pass, ...rest} = validUser._doc;
+        res.cookie('access_token', token, {htppOnly: true}).status(200).json(rest)
+    } catch (error) {
+        next(error)
+    }
+}
