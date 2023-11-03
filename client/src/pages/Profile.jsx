a447f5797855bd6406085610ffe33c7dbe10ecce
Jose <josecrsg@gmail.com>
2023-11-03 17:56:06 +0100

    complete delete user listing functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef, useEffect } from 'react'
 import { useSelector } from 'react-redux'
-import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import {getDownloadURL, getStorage, list, ref, uploadBytesResumable} from 'firebase/storage'
 import { app } from '../firebase'
 import { deleteUserFailure, deleteUserStart, deleteUserSuccess, signInFailure, signOutUserFailure, signOutUserStart, signOutUserSuccess, updateUserFailure, updateUserStart, updateUserSuccess } from '../redux/user/userSlice'
 import { useDispatch } from 'react-redux'
@@ -21,6 +21,8 @@ export default function Profile () {
   const [fileUploadError, setFileUploadError] = useState(false)
   const [formData, setFormData] = useState({})
   const [updateSuccess, setUpdateSuccess] = useState(false)
+  const [showListingsError, setShowListingsError] = useState(false)
+  const [userListings, setUserListings] = useState([])
   const dispatch = useDispatch()
 
 
@@ -113,7 +115,40 @@ export default function Profile () {
     }
   }
 
-  
+  const handleShowListings = async () =>{
+    try {
+      setShowListingsError(false)
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json()
+      if(data.success === false){
+        setShowListingsError(true)
+        return
+      } 
+      
+      setUserListings(data)
+    } catch (error) {
+      setShowListingsError(true)
+    }
+  }
+
+  const handleListingDelete = async(listingId)=>{
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method: 'DELETE'
+      })
+      const data = await res.json()
+      if(data.success === false){
+        console.log(data.message)
+        return
+      }
+
+      setUserListings((prev) => 
+      prev.filter((listing)=> listing._id !== listingId))
+    } catch (error) {
+      
+    }
+
+  }
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
@@ -166,6 +201,38 @@ export default function Profile () {
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
       <p className='text-green-700 mt-5'>{updateSuccess ? 'User is updated successfully' :  ''}</p>
+      <button onClick={handleShowListings} className='text-green-700 w-full '>Show Listings</button>
+      <p className='text-red-700 mt-5'>{showListingsError ? 'Error showing listings' : ''}</p>
+
+      {userListings &&
+       userListings.length > 0 &&
+       <div className='flex flex-col gap-4'>
+        <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>  
+       {userListings.map((listing)=>(
+        <div key={listing._id} className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+          <Link to={`/listing/${listing._id}`}>
+            <img 
+              src={listing.imageUrls[0]} 
+              alt="listing cover"
+              className='h-16 w-16 object-contain' 
+            />
+          </Link>
+          <Link to={`/listing/${listing._id}`} className='text-slate-700 font-semibold hover:underline truncate flex-1'>
+            <p className=''>
+              {listing.name}
+            </p>          
+          </Link>
+
+          <div className='flex flex-col items-center'>
+            <button onClick={()=>handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
+            <button className='text-green-700 uppercase'>Edit</button>
+
+
+          </div>
+        </div>
+       ))}
+       </div>
+      }
     </div>
   )
 }
