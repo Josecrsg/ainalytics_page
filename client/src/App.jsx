bb5d4f699ab7c0c47983ecdc4d27ced6020da8b2
Jose <josecrsg@gmail.com>
2023-11-05 18:44:40 +0100

    image slider listing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import { UpdateListing } from './pages/UpdateListing';
 import { CreateListing } from './pages/CreateListing';
+import Listing from './pages/Listing'
 
 export default function App(){
   return <BrowserRouter>
@@ -15,8 +16,9 @@ export default function App(){
   <Routes>
     <Route path='/' element={<Home />} />
     <Route path='/sign-in' element={<SignIn />} />
-    <Route path='/about' element={<About />} />
     <Route path='/sign-up' element={<SignUp />} />    
+    <Route path='/about' element={<About />} />
+    <Route path='/listing/:listingId' element={<Listing />} />    
     <Route element={<PrivateRoute />} >
       <Route path='/profile' element={<Profile />} />
       <Route path='/create-listing' element={<CreateListing />} />
