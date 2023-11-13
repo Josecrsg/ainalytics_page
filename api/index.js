89e1d7bc7e6829526ac830f662d1c639726e0e90
Jose <josecrsg@gmail.com>
2023-11-13 22:07:51 +0100

    fixed

--- api/index.js
+++ api/index.js
@@ -1,51 +1,63 @@
+// Importar los módulos necesarios
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
-import cookieParser from 'cookie-parser';
+import contactRouter from './routes/contact.routes.js';
 import path from 'path';
+
+// Cargar las variables de entorno desde un archivo .env
 dotenv.config();
 
+// Conectar a la base de datos MongoDB
 mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('Connected to MongoDB!');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('Conectado a MongoDB');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
-  const __dirname = path.resolve();
+    //Crear un static folder
+    const __dirname = path.resolve();
 
+// Crear una instancia de la aplicación Express
 const app = express();
 
+// Configurar middleware para el análisis de JSON en las solicitudes
 app.use(express.json());
 
+// Configurar middleware para el análisis de cookies en las solicitudes
 app.use(cookieParser());
 
+// Iniciar el servidor Express y escuchar en el puerto 3000
 app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+    console.log('El servidor está funcionando en el puerto 3000!!!!');
 });
 
+// Definir rutas para las diferentes partes de la aplicación
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
-
+app.use('/api/contact', contactRouter);
 
 app.use(express.static(path.join(__dirname, '/client/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  })
 
+/**Aquí se está creando un middleware para gestionar los errores */
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  return res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
-})
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Error interno del servidor';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
